3c96f7894b78de99611a779caac1733f277dbf06
Nonprawich Intakaew <40120130+npwitk@users.noreply.github.com>
2025-06-24 22:36:38 +0700

    Initial Commit

---
+++ StoreKit-Paywall/StoreKit_PaywallApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreKit_PaywallApp.swift
+//  StoreKit-Paywall
+//
+//  Created by Nonprawich I. on 24/6/25.
+//
+
+import SwiftUI
+
+@main
+struct StoreKit_PaywallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
